5629be830306b0828bef608f8c107f58af4af329
Sandya Sivakumar <sandya.sandy.s@gmail.com>
2020-02-06 14:32:56 +0530

    Chapter 2 files updated

--- src/main/java/com/cisco/javatwo/chapter2/BMICalculator.java
+++ src/main/java/com/cisco/javatwo/chapter2/BMICalculator.java
@@ -1,5 +1,28 @@
 package com.cisco.javatwo.chapter2;
 
-public class BMICalculator {
+import java.util.Scanner;
 
+public class BMICalculator {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner((System.in));
+        System.out.println("Enter weight in kilograms");
+        float weight = scanner.nextFloat();
+        System.out.println("Enter height in meters");
+        float height = scanner.nextFloat();
+
+        float bmi = weight/(height*height);
+        System.out.println("BMI :"+bmi);
+        if(bmi<18.5){
+            System.out.println("Underweight");
+        }
+        else if(bmi>=18.5 && bmi<25.0){
+            System.out.println("Normal");
+        }
+        else if(bmi>=25 && bmi<30.0){
+            System.out.println("Overweight");
+        }
+        else{
+            System.out.println("Obese");
+        }
+    }
 }
